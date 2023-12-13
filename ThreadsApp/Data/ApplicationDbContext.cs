5f775b584ca86b536c1df26663968309dd86dc7a
anarosu <anarosu766@gmail.com>
2023-12-13 21:40:33 +0200

    sistem de autentificare part1

--- ThreadsApp/Data/ApplicationDbContext.cs
+++ ThreadsApp/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ThreadsApp.Models;
 
 namespace ThreadsApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
---
+++ ThreadsApp/Models/ApplicationUser.cs
@@ -0,0 +1,6 @@
+﻿namespace ThreadsApp.Models
+{
+    public class ApplicationUser
+    {
+    }
+}
